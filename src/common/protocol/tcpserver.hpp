401c47b1938f4cb0ae31236993c141d4bf01cfff
Alex Davydow <adavydow@yandex.ru>
2012-07-22 20:21:36 +0400

    garbage removed

--- src/common/protocol/tcpserver.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _TMS_COMMON_PROTOCOL__TCP_SERVER_HPP_
-#define _TMS_COMMON_PROTOCOL__TCP_SERVER_HPP_
-class TCPCServer : public Server {
-  
-}
-#eval // _TMS_COMMON_PROTOCOL__TCP_SERVER_HPP_
