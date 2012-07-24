7591ff35ef0871af626e52ef187fcb5e3ac4195a
Mariya <mariyafomkina@gmail.com>
2012-07-24 22:17:17 +0400

    ContraptionGridTableBase appeared

--- src/common/widget/contraption_grid.cpp
+++ src/common/widget/contraption_grid.cpp
@@ -13,46 +13,19 @@ ContraptionGrid::ContraptionGrid(ContraptionArrayP contraptions,
                                  const wxPoint &pos, const wxSize &size,
                                  long style, const wxString &name) :
     wxGrid(parent, id, pos, size, style, name),
-    parent_(parent),
-    contraptions_(contraptions), model_(), cols_(cols),
-    on_cell_click_(), on_cell_dclick_(),
-    contraprions_drawn_(), printer_() {
+    contraptions_(contraptions), cols_(cols),
+    on_cell_click_(), on_cell_dclick_() {
   EnableDragColSize();
   EnableDragColMove();
   EnableDragRowSize();
   DisableCellEditControl();
   EnableEditing(false);
-  LoadData();
-  BindListeners();
-}
-
-void ContraptionGrid::LoadData() {
-  size_t cols_number = cols_.size();
-  size_t rows_number = contraptions_->size();
-  model_ = contraptions_->model();
-
-  CreateGrid(rows_number, cols_number, wxGridSelectRows);
-  for (size_t i = 0; i < rows_number; ++i) {
-    contraprions_drawn_.push_back(false);
-  }
-
-  for (size_t j = 0; j < cols_number; j++) {
-    FieldID i = cols_[j].field_id;
-    if (dynamic_cast<const FieldT<int>*>(model_->GetField(i)) != 0) {
-      printer_.push_back(boost::shared_ptr<Printer>(new PrinterT<int>()));
-    } else if (dynamic_cast<const FieldT<std::string>*>(model_->GetField(i)) != 0) {
-      printer_.push_back(boost::shared_ptr<Printer>(new PrinterT<std::string>()));
-    }
-    SetColLabelValue(j, cols_[j].name);
+  ContraptionGridTableBase *base =
+    new ContraptionGridTableBase(contraptions_, cols_);
+  SetTable(base, true, wxGridSelectRows);
+  for (size_t j = 0; j < cols.size(); j++) {
     SetColSize(j, cols_[j].width);
   }
-}
-
-void ContraptionGrid::BindListeners() {
-  parent_->Bind(wxEVT_MOVE, &ContraptionGrid::OnMove, this);
-  parent_->Bind(wxEVT_MOVING, &ContraptionGrid::OnMove, this);
-  Bind(wxEVT_PAINT, &ContraptionGrid::OnPaint, this);
-  Bind(wxEVT_SIZE, &ContraptionGrid::OnSize, this);
   Bind(wxEVT_GRID_CELL_LEFT_CLICK, &ContraptionGrid::OnCellClick, this);
   Bind(wxEVT_GRID_CELL_LEFT_DCLICK, &ContraptionGrid::OnCellDClick, this);
   contraptions_->SetOnChange(boost::bind(&ContraptionGrid::OnChange, this));
@@ -67,54 +40,6 @@ void ContraptionGrid::SetOnCellClick(OnClickFunction on_cell_click) {
 
 void ContraptionGrid::SetOnCellDClick(OnClickFunction on_cell_dclick) {
   on_cell_dclick_.connect(on_cell_dclick);
-}
-
-void ContraptionGrid::DrawContent(int min_row, int max_row) {
-  size_t cols_number = cols_.size();
-  for (size_t i = min_row; i < max_row; i++) {
-    if (!contraprions_drawn_[i]) {
-      for (FieldID j = 0; j < cols_number; j++) {
-        if (model_->GetField(cols_[j].field_id)->IsReadable()) {
-          SetCellValue(i, j, printer_[j]->ToString(*(contraptions_->at(i)->
-                                                       GetFieldValue(cols_[j].field_id))));
-        } else {
-          SetCellValue(i, j, _T("###"));
-        }
-      }
-      contraprions_drawn_[i] = true;
-    }
-  }
-}
-
-void ContraptionGrid::OnUpdateView() {
-  ForceRefresh();
-  int x = 0;
-  int y = 0;
-  CalcUnscrolledPosition(0, 0, &x, &y);
-  int min_row = YToRow(y);
-  int w = 0;
-  int h = 0;
-  GetSize(&w, &h);
-  CalcUnscrolledPosition(0, h, &x, &y);
-  int max_row = YToRow(y);
-  if (max_row == -1) max_row = contraptions_->size();
-  DrawContent(min_row, max_row);
-}
-
-void ContraptionGrid::OnMove(wxMoveEvent &WXUNUSED(e)) {
-  OnUpdateView();
-}
-
-void ContraptionGrid::OnPaint(wxPaintEvent &WXUNUSED(e)) {
-  OnUpdateView();
-}
-
-void ContraptionGrid::OnSize(wxSizeEvent &WXUNUSED(e)) {
-  OnUpdateView();
-}
-
-void ContraptionGrid::OnScrollWin(wxScrollWinEvent &WXUNUSED(e)) {
-  OnUpdateView();
 }
 
 void ContraptionGrid::OnCellClick(wxGridEvent &e) {
@@ -136,18 +61,7 @@ void ContraptionGrid::OnCellDClick(wxGridEvent &e) {
 }
 
 void ContraptionGrid::OnChange() {
-  size_t rows_number = contraptions_->size();
-  contraprions_drawn_.clear();
-  for (size_t i = 0; i < rows_number; ++i) {
-    contraprions_drawn_.push_back(false);
-  }
-  int delta = rows_number - GetNumberRows();
-  if (delta > 0) {
-    AppendRows(delta);
-  } else if (delta < 0) {
-    DeleteRows(rows_number, delta);
-  }
-  OnUpdateView();
+  ForceRefresh();
 }
 
 }
---
+++ src/common/widget/contraption_grid_table_base.hpp
@@ -0,0 +1,52 @@
+#ifndef _TMS_COMMON_WIDGET__CONTRAPTION_GRID_TABLE_BASE_HPP_
+#define _TMS_COMMON_WIDGET__CONTRAPTION_GRID_TABLE_BASE_HPP_
+
+// wxWidget
+#include <wx/grid.h>
+#include <wx/event.h>
+// boost
+#include <boost/signals.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+// std
+#include <cstdio>
+#include <vector>
+// common
+#include <contraption/model.hpp>
+#include <contraption/contraption_array.hpp>
+#include <contraption/field.hpp>
+#include <contraption/field_type.hpp>
+#include "printer.hpp"
+#include "column.hpp"
+
+namespace tms {
+namespace common {
+namespace widget {
+
+using namespace contraption;
+
+class ContraptionGridTableBase : public wxGridTableBase {
+ public:
+  ContraptionGridTableBase(ContraptionArrayP contraptions,
+                           std::vector<Column> &cols);
+  ~ContraptionGridTableBase() {}
+
+  wxString GetValue(int row, int col);
+  void SetValue(int row, int col, const wxString &value);
+  int GetNumberCols();
+  int GetNumberRows();
+  wxString GetColLabelValue(int col);
+
+ private:
+  ContraptionArrayP contraptions_;
+  ModelP model_;
+  std::vector<Column> cols_;
+  std::vector<boost::shared_ptr<Printer> > printer_;
+
+};
+
+}
+}
+}
+#endif // _TMS_COMMON_WIDGET__CONTRAPTION_GRID_TABLE_BASE_HPP_
