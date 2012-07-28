78e1db590118ff64f35d54348b9f61b79c6e31e2
Alex Davydow <adavydow@yandex.ru>
2012-07-28 06:16:58 +0400

    quick and dirty form fix

--- src/client/test_frame.cpp
+++ src/client/test_frame.cpp
@@ -21,22 +21,23 @@ void TestFrame::Init() {
   Bind(wxEVT_CLOSE_WINDOW, &TestFrame::OnTryClose, this);
 }
 
-void TestFrame::SetUpValues(ContraptionP contraption, ContraptionArrayP contraptions) {
+void TestFrame::SetUpValues(ContraptionP contraption, 
+                            ContraptionArrayP contraptions) {
   contraption_ = contraption;
   contraptions_ = contraptions;
   tc_name_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL1");
   tc_name_->ChangeValue(contraption_->Get<std::string>("name"));
   tc_surname_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL2");
-  tc_surname_->ChangeValue(contraption_->Get<std::string>("Surname"));
+  tc_surname_->ChangeValue(contraption_->Get<std::string>("phone"));
   tc_age_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL3");
   tc_age_->ChangeValue("");
-  *tc_age_ << contraption_->Get<int>("age");
+  *tc_age_ << contraption_->Get<std::string>("email");
 }
 
 void TestFrame::OnSaveClick(wxCommandEvent& WXUNUSED(event)) {
   contraption_->Set<std::string>("name", tc_name_->GetValue().ToStdString());
-  contraption_->Set<std::string>("Surname", tc_surname_->GetValue().ToStdString());
-  contraption_->Set<int>("age", boost::lexical_cast<int>(tc_age_->GetValue().ToStdString()));
+  contraption_->Set<std::string>("phone", tc_surname_->GetValue().ToStdString());
+  contraption_->Set<std::string>("email", tc_age_->GetValue().ToStdString());
   contraption_->Save();
   contraptions_->Refresh();
   Hide();
