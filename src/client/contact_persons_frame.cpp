c68f8ed7d36493405044727a64f2c4349c84dd09
Alex Davydow <adavydow@yandex.ru>
2012-07-30 04:20:45 +0400

    Issue 14

--- src/client/contact_persons_frame.cpp
+++ src/client/contact_persons_frame.cpp
@@ -26,26 +26,32 @@ void ContactPersonsFrame::SetUpValues(ContraptionP contraption,
   contraption_ = contraption;
   contraptions_ = contraptions;
   tc_name_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL1");
-  tc_name_->ChangeValue(contraption_->Get<std::string>("name"));
+  tc_name_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("name").c_str()));
   tc_code_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL2");
-  tc_code_->ChangeValue(contraption_->Get<std::string>("code"));
+  tc_code_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("code").c_str()));
   tc_email_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL3");
-  tc_email_->ChangeValue(contraption_->Get<std::string>("email"));
+  tc_email_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("email").c_str()));
   tc_phone_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL4");
-  tc_phone_->ChangeValue(contraption_->Get<std::string>("phone"));
+  tc_phone_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("phone").c_str()));
   tc_fax_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL5");
-  tc_fax_->ChangeValue(contraption_->Get<std::string>("fax"));
+  tc_fax_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("fax").c_str()));
   tc_notes_ = (wxTextCtrl*)FindWindowByName("ID_TEXTCTRL6");
-  tc_notes_->ChangeValue(contraption_->Get<std::string>("note"));
+  tc_notes_->ChangeValue(wxString::FromUTF8(
+      contraption_->Get<std::string>("note").c_str()));
 }
 
 void ContactPersonsFrame::OnSaveClick(wxCommandEvent& WXUNUSED(event)) {
-  contraption_->Set<std::string>("name", tc_name_->GetValue().ToStdString());
-  contraption_->Set<std::string>("code", tc_code_->GetValue().ToStdString());
-  contraption_->Set<std::string>("email", tc_email_->GetValue().ToStdString());
-  contraption_->Set<std::string>("phone", tc_phone_->GetValue().ToStdString());
-  contraption_->Set<std::string>("fax", tc_fax_->GetValue().ToStdString());
-  contraption_->Set<std::string>("note", tc_notes_->GetValue().ToStdString());
+  contraption_->Set<std::string>("name", tc_name_->GetValue().utf8_str().data());
+  contraption_->Set<std::string>("code", tc_code_->GetValue().utf8_str().data());
+  contraption_->Set<std::string>("email", tc_email_->GetValue().utf8_str().data());
+  contraption_->Set<std::string>("phone", tc_phone_->GetValue().utf8_str().data());
+  contraption_->Set<std::string>("fax", tc_fax_->GetValue().utf8_str().data());
+  contraption_->Set<std::string>("note", tc_notes_->GetValue().utf8_str().data());
   contraption_->Save();
   contraptions_->Refresh();
   Hide();
